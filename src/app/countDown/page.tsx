ba6ac3ed1961cf7a4c2a2bfe655240e2cb4ab24f
u2-ow <utwo4805@gmail.com>
2023-05-20 15:09:16 +0900

    :sparkles: 問題開始前のカウントダウン機能を追加

---
+++ src/app/countDown/page.tsx
@@ -0,0 +1,28 @@
+'use client'
+import { useRouter } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
+
+type Props = {}
+
+export default function Page({}: Props) {
+    const [count,setCount] = useState(3);
+    const router = useRouter();
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+          setCount((prev) => prev - 1);
+        }, 1000);
+        if (count === 0) {
+          clearInterval(interval);
+          console.log('START')
+          router.push("/quiz/seo/q1")
+        }
+        return () => clearInterval(interval);
+      }, [count]);
+
+  return (
+    <div>
+        <p>{count}</p>
+    </div>
+  )
+}
