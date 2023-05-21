51fe2748fd1157059455403ba3180a60c64f5ed7
u2-ow <utwo4805@gmail.com>
2023-05-21 23:41:31 +0900

    :sparkles: seo用の問題を取得できるようにした

---
+++ utils/fetchSeoQuestion.ts
@@ -0,0 +1,9 @@
+import supabase from "./supabase";
+
+
+
+export default async function fetchSeoQuestion() {
+    const { data: questions } = await supabase.from('randam_seo_question').select('*').limit(1);
+    console.log(questions);
+    return questions
+  }
