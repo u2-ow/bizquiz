230482295f9b20be8e72feff6d9b1dac97bfd305
u2-ow <utwo4805@gmail.com>
2023-05-23 22:44:00 +0900

    :recycle: 定数を直した

--- utils/fetchSeoChoice.ts
+++ utils/fetchSeoChoice.ts
@@ -3,7 +3,7 @@ import supabase from "./supabase";
 
 
 export default async function fetchSeoChoice(questionId) {
-    const { data: questions } = await supabase.from('random_seo_choice').select('*').eq('question_id', questionId)
+    const { data: choices } = await supabase.from('random_seo_choice').select('*').eq('question_id', questionId)
     console.log(questionId);
-    return questions
+    return choices
   }
