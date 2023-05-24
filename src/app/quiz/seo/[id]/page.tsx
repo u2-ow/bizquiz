6745fea32a72c8ca6b5a8e6469449c50245da124
u2-ow <utwo4805@gmail.com>
2023-05-24 22:06:48 +0900

    :sparkles: スコアの減算機能を追加

--- src/app/quiz/seo/[id]/page.tsx
+++ src/app/quiz/seo/[id]/page.tsx
@@ -4,6 +4,7 @@
 import { useEffect, useState } from "react";
 import fetchSeoQuestion from "../../../../../utils/fetchSeoQuestion";
 import fetchSeoChoice from "../../../../../utils/fetchSeoChoice";
+import { useQuizCountDown } from "@/hooks/useQuizCountDown";
 
 
 type Question = {
@@ -20,22 +21,48 @@ type Choice = {
 export default function Page() {
   const [seoQuestions,setSeoQuestions] = useState<Question[]>([])
   const [seoChoices,setSeoChoices] = useState<Choice[]>([]);
-  //setSeoQuestionsが非同期の関数じゃないからuseEffect内で非同期関数を呼び出さないといけない
+  const quizTimer = useQuizCountDown();
+
   useEffect(()=>{
   const fetchData = async () => {
     const questions = await fetchSeoQuestion();
-
     setSeoQuestions(questions as Question[]);
-    const questionId = questions[0].id
+    const questionId = questions[0].id;
     console.log(questionId)
     const choices = await fetchSeoChoice(questionId);
-    console.log(choices)
-
     setSeoChoices(choices as Choice[])
   };
-
   fetchData();
   },[])
+
+
+  const judege = (e: { target: { innerHTML: string; }; })=>{
+    console.log(seoChoices)
+    const selectedText = e.target.innerHTML;
+    const selectedChoices = seoChoices.find((item) => item.choice_text === selectedText);
+    if(selectedChoices?.is_correct !== true){
+      console.log('不正解')
+      console.log(sessionStorage)
+      const userScore = Number(sessionStorage.getItem('defaultScore') || 10);
+      sessionStorage.setItem('defaultScore', String(userScore - 1));
+      console.log(sessionStorage.defaultScore)
+      return
+
+    }else{
+            sessionStorage.clear();
+    }
+    // else{
+    //   console.log('不正解')
+    //   const currentScore = sessionStorage.getItem('defaultScore');
+    //   const updatedScore = parseInt(currentScore, 10) - 1;
+    //   console.log(updatedScore);
+    //   sessionStorage.setItem('defaultScore', updatedScore.toString());
+    //   console.log(sessionStorage)
+    // }
+  }
+
+
+
  
   return (
     <>
@@ -48,12 +75,12 @@ export default function Page() {
     }
        {
            seoChoices.map((seoChoice) => (
-            <p key={seoChoice.id}>
+            <p key={seoChoice.id} onClick={judege}>
               {seoChoice.choice_text}
             </p>
           ))
     }
-  
+          <p>{quizTimer}</p>
 
     </>
   )
