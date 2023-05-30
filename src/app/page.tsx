a360deb56e4d65895d0b92bb7ee905564e417535
u2-ow <utwo4805@gmail.com>
2023-05-30 23:12:40 +0900

    :construction:見た目を整え中

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,20 +4,30 @@ import { creatUserData } from "../../utils/creatUserData"
 
 
 
-
-
 export default function Home() {
  creatUserData();
   return (
     <>
-      <div>
-        <h1>bizquiz</h1>
-        <ul>
-          <li>Marketing</li>
-          <li><Link href='/countDown'>SEO</Link></li>
-          <li>Programming</li>
+    <header>
+    <Link href="/" className="quit">やめる</Link>
+    </header>
+    <main >
+    <h1>
+      <span className="text-blue">b</span>
+      <span className="text-yellow">i</span>
+      <span className="text-red">z</span>
+      <span className="text-green">q</span>
+      <span className="text-yellow">u</span>
+      <span className="text-blue">i</span>
+      <span className="text-red">z</span>
+    </h1>
+    <p>クイズのカテゴリを選択してください。</p>
+        <ul className="mode-list">
+          <li className="mode-list__item"><Link href='/countDown'>SEO</Link></li>
+          <li className="mode-list__item">Marketing</li>
         </ul>
-      </div>
+    </main>
+
     </>
   )
 }
