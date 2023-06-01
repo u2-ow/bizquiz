48bb9bd93987c2af54bb25a1ce23503897ce996c
u2-ow <utwo4805@gmail.com>
2023-06-01 22:56:50 +0900

    :recycle: フォントファミリーの付与の

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={DotGothic16_normal.className}>{children}</body>
+    <body className={DotGothic16_normal.className}>{children}</body>
     </html>
   )
 }
