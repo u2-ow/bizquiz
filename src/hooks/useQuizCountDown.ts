2a6b3b799345959257a44c1146be6788ecc82f56
u2-ow <utwo4805@gmail.com>
2023-06-04 18:26:49 +0900

    :construction :カウント調整中

--- src/hooks/useQuizCountDown.ts
+++ src/hooks/useQuizCountDown.ts
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react";
+import { usePathname,useRouter } from 'next/navigation';
 export const useQuizCountDown = ()=>{
-    const [quizTimer,setQuizTimer] = useState(5);
+    const [quizTimer,setQuizTimer] = useState(1);
     useEffect(()=>{
         if (quizTimer === 0) {
-            console.log('HEHEHEHEH');
-            return;
-          }
-      
+          return
+        }
           const countDown = setTimeout(() => {
             setQuizTimer((prev) => prev - 1);
           }, 1000);
       
           return () => clearTimeout(countDown);
         }, [quizTimer]);
-      
         return quizTimer;   
 }
+
+
+
