f3ee396acd9a456678cf9130a1923ddef381e579
u2-ow <utwo4805@gmail.com>
2023-06-08 23:16:28 +0900

    :recycle:型を定義した。

--- utils/fetchSeoChoice.ts
+++ utils/fetchSeoChoice.ts
@@ -1,8 +1,9 @@
+import { FC } from "react";
 import supabase from "./supabase";
 
 
 
-export default async function fetchSeoChoice(questionId) {
+export default async function fetchSeoChoice(questionId:number) {
     const { data: choices } = await supabase.from('random_seo_choice').select('*').eq('question_id', questionId)
     console.log(questionId);
     return choices
