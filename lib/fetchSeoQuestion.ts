4060e342b606395c8ce146563500549a6082c7c4
u2-ow <utwo4805@gmail.com>
2023-06-13 23:36:11 +0900

    :truck: utilsからlibに引っ越した

--- lib/fetchSeoQuestion.ts
+++ lib/fetchSeoQuestion.ts
@@ -3,7 +3,7 @@ import supabase from "./supabase";
 
 
 export default async function fetchSeoQuestion() {
-    const { data: questions } = await supabase.from('randam_seo_question').select('*').limit(1);
+    const { data: questions } = await supabase.from('randam_seo_question').select('*').limit(10);
     console.log(questions);
     return questions;
   }
