7125b2346d7fe63dd8029ebe2182653f42e7458a
u2-ow <utwo4805@gmail.com>
2023-06-27 23:17:24 +0900

    :fire:検証用のコードを削除した

--- src/lib/fetchSeoChoice.ts
+++ src/lib/fetchSeoChoice.ts
@@ -4,6 +4,5 @@ import supabase from "./supabase";
 
 export default async function fetchSeoChoice(questionId:number) {
     const { data: choices } = await supabase.from('random_seo_choice').select('*').eq('question_id', questionId)
-    console.log(questionId);
     return choices
   }
--- src/lib/fetchSeoQuestion.ts
+++ src/lib/fetchSeoQuestion.ts
@@ -4,6 +4,5 @@ import supabase from "./supabase";
 
 export default async function fetchSeoQuestion() {
     const { data: questions } = await supabase.from('randam_seo_question').select('*').limit(1);
-    console.log(questions);
     return questions;
   }
