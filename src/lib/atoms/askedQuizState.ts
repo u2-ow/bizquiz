d87e66f7366cd69cad194f678fb2a6c895719a75
u2-ow <utwo4805@gmail.com>
2023-06-27 23:17:49 +0900

    :recycle:型を定義した

--- src/lib/atoms/askedQuizState.ts
+++ src/lib/atoms/askedQuizState.ts
@@ -1,6 +1,7 @@
 import { atom } from "recoil";
+import { Question } from "@/types/question";
 
-export const askedQuizState = atom({
+export const askedQuizState = atom<Question[]>({
     key: "askedQuizState",
     default: [],
   });
