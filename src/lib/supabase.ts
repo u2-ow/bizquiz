abd272b86cfcf10bd82d895bbcd65df8bd2dc76c
u2-ow <utwo4805@gmail.com>
2023-07-23 10:36:45 +0900

    修正

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+
+const supabaseUrl = 'https://jhpcnxpbqfqeociocoan.supabase.co'
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+const supabase = createClient(supabaseUrl, supabaseKey)
 export default supabase;
