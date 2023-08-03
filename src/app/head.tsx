4e105ec2921d6bc3132ab0c58a9f154a4841cfd9
u2-ow <utwo4805@gmail.com>
2023-08-03 23:05:30 +0900

    faviconの階層を変更

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -15,32 +15,20 @@ export default function Head(props:MetaPros) {
 
     return (
       <>
-          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-          <link rel="manifest" href="/favicons/site.webmanifest" />
-          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
-          <link rel="shortcut icon" href="/favicons/favicon.ico" /> 
-          <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="msapplication-config" content="/favicons/browserconfig.xml" /> 
-          <meta name="theme-color" content="#ffffff" />
-        {/* <title>bizquiz｜SEOやマーケティングで使用される用語の4択のクイズゲーム！</title>
 
-
-
+        <title>bizquiz｜SEOやマーケティングで使用される用語の4択のクイズゲーム！</title>
+        <meta name="description" content="SEOやマーケティングで使用される用語の4択のクイズゲーム"/> 
         <meta content="width=device-width, initial-scale=1" name="viewport" />
-        <meta name="description" content="SEOやマーケティングで使用される用語の4択のクイズゲーム"/>
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-          <link rel="manifest" href="/favicons/site.webmanifest" />
-          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
-          <link rel="shortcut icon" href="/favicons/favicon.ico" /> 
-          <meta name="msapplication-TileColor" content="#ffffff" />
-          <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
-          <meta name="theme-color" content="#ffffff" /> */}
-        {/* <meta name="msapplication-TileColor" content="#ffffff" />
-
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
+        <link rel="shortcut icon" href="/favicon.ico" /> 
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-config" content="/browserconfig.xml" /> 
+        <meta name="theme-color" content="#ffffff" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="theme-color" content="#ffffff" />
         <meta property="og:title" content="bizquiz"/>
         <meta property="og:type" content="application"/>
@@ -50,7 +38,7 @@ export default function Head(props:MetaPros) {
         <meta property="og:image" content="https://www.bizquiz.app/bizquiz.jpg"/>
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@u2_oww" />
-        <meta name="twitter:player" content="@u2_oww" /> */}
+        <meta name="twitter:player" content="@u2_oww" />
 
       </>
     )
