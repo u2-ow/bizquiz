3a99b3332313bb4c079bc106bbe16d1c671eb10d
u2-ow <utwo4805@gmail.com>
2023-08-03 21:56:34 +0900

    favicon修正

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -16,16 +16,20 @@ export default function Head(props:MetaPros) {
     return (
       <>
         <title>bizquiz｜SEOやマーケティングで使用される用語の4択のクイズゲーム！</title>
+
+
         <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-        <link rel="manifest" href="/favicons/site.webmanifest" />
-        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
-        <link rel="shortcut icon" href="/favicons/favicon.ico" /> 
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#000000" />
+          <link rel="shortcut icon" href="/favicons/favicon.ico" /> 
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-config" content="/favicons/browserconfig.xml" /> 
+          <meta name="theme-color" content="#ffffff" />
+
         <meta content="width=device-width, initial-scale=1" name="viewport" />
         <meta name="description" content="SEOやマーケティングで使用される用語の4択のクイズゲーム"/>
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
         <meta name="theme-color" content="#ffffff" />
         <meta property="og:title" content="bizquiz"/>
         <meta property="og:type" content="application"/>
